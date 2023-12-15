6204d248d4fba6695e82eb0c76bb197b3e0f7a5f
NJ-TK <njtk48@gmail.com>
2023-12-15 12:09:24 +0900

    Netlify Functionsからサーバー上のJSONファイルにアクセスできることを確認

--- netlify/functions/hello-world/hello-world.ts
+++ netlify/functions/hello-world/hello-world.ts
@@ -1,15 +1,34 @@
 import type { Handler } from '@netlify/functions'
+import fs from 'fs'
+import path from 'path'
 
 // any型の追加, _contextへの書き換え
 export const handler: Handler = async (event: any, _context: any) => {
   // 追加
   const query: string = await event.queryStringParameters.q || 'No query'
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      // queryに書き換え
-      message: `Hello, ${query}!`
-    })
+  // JSONを読んでみる
+  try {
+    // サーバー上のJSONファイルのパス
+    const jsonFilePath = path.resolve(__dirname, '../hoge.json');
+
+    // JSONファイルを読み込む
+    const jsonData = fs.readFileSync(jsonFilePath, 'utf-8');
+
+    const data = JSON.parse(jsonData)
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+        // queryに書き換え
+        message: `Hello, ${query}!, ${data.foo}`
+        })
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
   }
+
 }
